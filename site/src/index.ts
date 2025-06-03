bcdf8853c1a8bb62027dc5e72f6b21b1d3ad60b8
Ben Allfree <ben@benallfree.com>
2025-06-03 05:42:39 -0700

    feat: add MyDurableObject2 and update instances

--- site/src/index.ts
+++ site/src/index.ts
@@ -17,6 +17,19 @@ export class MyDurableObject extends DurableObject<Env> {
   }
 }
 
+export class MyDurableObject2 extends DurableObject<Env> {
+  private fs: Fs
+
+  constructor(ctx: DurableObjectState, env: Env) {
+    super(ctx, env)
+    this.fs = new Fs(ctx, env, { chunkSize: 4 * 1024 })
+  }
+
+  public getFs() {
+    return this.fs
+  }
+}
+
 const app = new Hono<{ Bindings: Env }>()
 
 // Mount the API middleware
@@ -28,7 +41,19 @@ app.route(
       MY_DURABLE_OBJECT: {
         name: 'My Durable Object',
         async getInstances(page = 1) {
-          return [{ slug: 'demo', name: 'Demo Instance' }]
+          return [
+            { slug: 'instance1', name: 'Instance 1' },
+            { slug: 'instance2', name: 'Instance 2' },
+          ]
+        },
+      },
+      MY_DURABLE_OBJECT_2: {
+        name: 'My Durable Object 2',
+        async getInstances(page = 1) {
+          return [
+            { slug: 'instance1', name: 'Instance 1' },
+            { slug: 'instance2', name: 'Instance 2' },
+          ]
         },
       },
     },
